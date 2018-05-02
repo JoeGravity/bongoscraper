726aafd7ad3137d0c928a9a9aab0d07d43b6295b
Joe Pfahl <joegravity@gmail.com>
2018-05-02 07:03:01 -0500

    Added mLab connection for Heroku

--- server.js
+++ server.js
@@ -27,7 +27,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/bongodb");
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/bongodb";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
